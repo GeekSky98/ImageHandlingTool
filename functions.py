579a66f8c8d214890f5ebbe49799ca73d5be4bb0
Deepsky <deepskyai1215@gmail.com>
2023-01-04 22:15:56 +0900

    ADD class CustomSchedule for learning rate warmup and cosine decay

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from tensorflow import keras
 
 def show(image):
     plt.imshow(image)
@@ -11,3 +12,17 @@ def gray_show(image):
     plt.grid(True)
     plt.colorbar()
     plt.show()
+
+# Make learning rate warmup and cosine decay
+class CustomSchedule(keras.optimizers.schedules.LearningRateSchedule):
+  def __init__(self, max_lr, warmup_steps, decay_steps):
+    super(CustomSchedule, self).__init__()
+    self.max_lr = max_lr
+    self.warmup_steps = warmup_steps
+    self.decay_steps = decay_steps
+
+  def __call__(self, step):
+    lr = tf.cond(step < self.warmup_steps,
+                 lambda: self.max_lr / self.warmup_steps * step,
+                 lambda: 0.5 * (1 + tf.math.cos(math.pi * (step - self.warmup_steps) / self.decay_steps)) * self.max_lr)
+    return lr
