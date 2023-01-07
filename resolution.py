21909d790ee0ab8a80dafc1c684abe39548e7251
deepsky1215 <deepskyai1215@gmail.com>
2023-01-07 23:13:36 +0900

    Make a dataset containing low-quality images.

--- resolution.py
+++ resolution.py
@@ -35,56 +35,40 @@ n_epoch = 100
 # Write directory address
 cur_dir = os.getcwd()
 data_dir = os.path.join(cur_dir, "landscape_data")
-x_dir = os.path.join(data_dir, "gray")
 y_dir = os.path.join(data_dir, "color")
 
 # Checking amount of data > 7129 / 7129
-x_image_files = [fname for fname in os.listdir(x_dir) if os.path.splitext(fname)[-1]=='.jpg']
-len(x_image_files)
 y_image_files = [fname for fname in os.listdir(y_dir) if os.path.splitext(fname)[-1]=='.jpg']
 len(y_image_files)
 
 # Remove Images of different size
-for path in x_image_files:
-  temp = np.array(Image.open(os.path.join(x_dir, path))).shape
-  if temp != (img_size, img_size):
-    os.remove(os.path.join(x_dir, path))
-  else:
-    continue
-
-# Update gray data list and check amount
-x_image_files = [fname for fname in os.listdir(x_dir) if os.path.splitext(fname)[-1]=='.jpg']
-len(x_image_files)
-
-# Contrast the datasets of color and gray images to prevent datasets are different situation
 for path in y_image_files:
-  if path not in x_image_files:
+  temp = np.array(Image.open(os.path.join(y_dir, path))).shape
+  if temp != (img_size, img_size):
     os.remove(os.path.join(y_dir, path))
   else:
     continue
 
-# Update color data list and check amount
-y_image_files = [fname for fname in os.listdir(y_dir) if os.path.splitext(fname)[-1]=='.jpg']
-len(y_image_files)
-
-# Additionally, reconfirm that there are files with different image sizes in the color datasets
-for path in y_image_files:
-  temp = np.array(Image.open(os.path.join(y_dir, path))).shape
-  if temp != (img_size, img_size, 3):
-    print(path)
-  else:
-    continue
-
 # Load data
-x = np.array([np.array(Image.open(os.path.join(x_dir, image))) for image in x_image_files])
-# To match with the shape of the color datasets, adjust shape of gray datasets
-x = x.reshape(-1, img_size, img_size, 1)
 y = np.array([np.array(Image.open(os.path.join(y_dir, image))) for image in y_image_files])
 print(f"x_shape / y_shape : {x.shape, y.shape}")
 
 # Data scailing between 0 and 1
-x, y = x/x.max(), y/y.max()
-print(x.shape, x.max(), x.min(), y.shape, y.max(),y.min())
+y = y/y.max()
+print(y.shape, y.max(),y.min())
+
+# Make a low-quality datasets
+train_down = np.array([resize(img, (75, 75)) for img in y])
+train_up = np.array([resize(img, (150, 150)) for img in train_down])
 
 # Split train dataset and validation dataset
-train_x, val_x, train_y, val_y = train_test_split(x, y, test_size = 0.1, shuffle = True, random_state = 32)
+train_x, val_x, train_y, val_y = train_test_split(train_up, y, test_size = 0.1, shuffle = True, random_state = 32)
+
+# Make a Customdataset
+train_ds = functions.Dataloader(train_x, train_y, n_batch, shuffle = True)
+validation_ds = functions.Dataloader(val_x, val_y, n_batch)
+
+# Using Customschedule class to make lr warmup, cosine decay
+steps_per_epoch = n_train // n_batch
+lr_schedule = functions.CustomSchedule(LR, 3 * steps_per_epoch, n_epoch * steps_per_epoch)
+
