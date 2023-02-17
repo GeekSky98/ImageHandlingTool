23168326eb48fe87bb54a85e1fd62f0bfff1b359
Deepsky <deepskyai1215@gmail.com>
2023-02-17 17:35:10 +0900

    Change super resolution model to EDSR

--- resolution.py
+++ resolution.py
@@ -16,7 +16,7 @@ from sklearn.model_selection import train_test_split
 import math
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 from keras.layers import Conv2D, Conv2DTranspose, Input, Dropout, MaxPool2D, concatenate, Activation, \
-  BatchNormalization, GlobalAvgPool2D, Flatten, Reshape, Dense, Average
+  BatchNormalization, GlobalAvgPool2D, Flatten, Reshape, Dense, Average, Lambda, Add, Rescaling
 from keras.models import Model, Sequential, load_model
 from tensorflow.keras.applications import *
 
@@ -30,7 +30,8 @@ n_train = 6500
 n_val = 7106 - 6500
 LR = 0.0001
 n_batch = 32
-n_epoch = 100
+n_epoch = 50
+channel = 3
 
 # Write directory address
 cur_dir = os.getcwd()
@@ -191,4 +192,94 @@ model_resolution2.fit(
   verbose = 1
 )
 
-model_resolution2.save("./resolution2.h5", include_optimizer = False)
+
+
+class EDSR_model_builder(Model):
+  def train_step(self, data):
+    x, y = data
+
+    with tf.GradientTape() as tape:
+      y_pred = self(x, training=True)
+      loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+
+    trainable_vars = self.trainable_variables
+    gradients = tape.gradient(loss, trainable_vars)
+
+    self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+    self.compiled_metrics.update_state(y, y_pred)
+
+    return {m.name: m.result() for m in self.metrics}
+
+  def predict_step(self, data):
+    x = tf.cast(tf.expand_dims(data, axis=0), tf.float32)
+    pred = self(x, training=False)
+    pred = tf.clip_by_value(pred, 0, 255)
+    pred = tf.round(pred)
+    pred = tf.cast(tf.squeeze(pred, axis=0), tf.uint8)
+
+    return pred
+
+
+def res_block(Input_layer, filter=64):
+  res_x = Conv2D(filters=filter, kernel_size=3, activation='relu', padding='same')(Input_layer)
+  res_x = Conv2D(filters=filter, kernel_size=3, padding='same')(res_x)
+  res_x = Lambda(function=lambda x: x * 0.1)(res_x)
+  result = Add()([Input_layer, res_x])
+
+  return result
+
+
+def upsampling(Input_layer, filter=64, scale=2, **sky):
+  up_x = Conv2D(filter * (scale ** 2), kernel_size=3, padding='same', **sky)(Input_layer)
+  up_x = tf.nn.depth_to_space(up_x, block_size=scale)
+  up_x = Conv2D(filter * (scale ** 2), kernel_size=3, padding='same', **sky)(up_x)
+  up_x = tf.nn.depth_to_space(up_x, block_size=scale)
+
+  return up_x
+
+
+def build_edsr(filter=64, num_res=16):
+  input = Input((None, None, channel))
+  x = Rescaling(scale=1.0 / 255.0)(input)
+  x = res_x = Conv2D(filter, kernel_size=3, padding='same')(x)
+
+  for _ in range(num_res):
+    res_x = res_block(res_x)
+
+  res_x = Conv2D(filter, kernel_size=3, padding='same')(res_x)
+  x = Add()([x, res_x])
+
+  up_x = upsampling(x)
+  output = Conv2D(channel, kernel_size=3, padding='same')(up_x)
+  output = Rescaling(255)(output)
+
+  model = EDSR_model_builder(input, output)
+
+  return model
+
+
+model = build_edsr()
+
+optimizer = keras.optimizers.Adam(
+  learning_rate=keras.optimizers.schedules.PiecewiseConstantDecay(boundaries=[5000], values=[1e-4, 5e-5])
+)
+
+def PSNR(y_true, y_pred):
+  return tf.image.psnr(y_true, y_pred, max_val=255)[0]
+
+def SSIM(y_true, y_pred):
+  return tf.image.ssim(y_true, y_pred, max_val=255)[0]
+
+model.compile(
+  loss='mae',
+  optimizer=optimizer,
+  metrics=[PSNR, SSIM]
+)
+
+model.fit(
+    train_ds,
+    validation_data=validation_ds,
+    epochs=n_epoch,
+    steps_per_epoch=300,
+    verbose=1
+)
